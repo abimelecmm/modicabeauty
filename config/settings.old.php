0a5a058a0e8695602fdcd64149f20f1413cf6fda
Jules Piccotti <julpi@jules.it>
2015-06-02 10:48:39 +0000

    initial commit comment

---
+++ config/settings.old.php
@@ -0,0 +1,16 @@
+<?php
+define('_DB_SERVER_', 'localhost:3306');
+define('_DB_NAME_', 'bitnami_prestashop');
+define('_DB_USER_', 'bn_prestashop');
+define('_DB_PASSWD_', 'e0d3519fe8');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheFs');
+define('_PS_CACHE_ENABLED_', '1');
+define('_COOKIE_KEY_', 'sh54e8S8AMBj9xBh8CocTUzaDecMtido2muI6853nBHEU7Y5yQUXTatx');
+define('_COOKIE_IV_', 'rZSUeVM6');
+define('_PS_CREATION_DATE_', '2015-01-27');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.0.11');
+define('_RIJNDAEL_KEY_', 'BaYFczvDmZuBOcPuhGX3cja2ciPWnpxk');
+define('_RIJNDAEL_IV_', 'F2MKmZS5hzzwRy/QKuHjsw==');
